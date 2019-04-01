ed64c797dc9b4951e0b0274ef2cef30d2791965e
Giorgio Zanni <g.zanni@hotmail.com>
2019-04-01 11:15:16 -0300

    ADD: bet placing logic

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,11 +1,16 @@
 class Game < ApplicationRecord
   include Picker
   before_save :set_result
+  after_create :new_round
   has_many :bets
   has_many :players, through: :bets
 
   def set_result
     self.result = pick_one(green: 2, red: 49, black: 49)
+  end
+
+  def new_round
+    Player.place_bet(self)
   end
 
   enum result: %i[green red black]
--- app/models/player.rb
+++ app/models/player.rb
@@ -4,4 +4,22 @@ class Player < ApplicationRecord
   validates :email, presence: true
   validates :email, uniqueness: true
   validates :wallet, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def self.place_bet(game)
+    all.each do |player|
+      Bet.create(
+        player: player,
+        game: game,
+        amount: player.bet_amount
+      )
+    end
+  end
+
+  def bet_amount
+    wallet <= 1000 ? wallet : (wallet * rand(bet_range)).to_i
+  end
+
+  def bet_range
+    0.08..0.15
+  end
 end
